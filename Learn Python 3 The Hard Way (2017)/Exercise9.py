10d8b3622dd7534067d1827f67a8b2c829ac1af0
ritu-shyam <61557586+ritu-shyam@users.noreply.github.com>
2021-02-02 01:13:25 +0530

    Add files via upload

---
+++ Learn Python 3 The Hard Way (2017)/Exercise9.py
@@ -0,0 +1,13 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+#Printing strings on multiple lines can make text more readable to humans.
+#With multiple lines, strings can be grouped into clean and orderly text, formatted as a letter, or used to maintain the linebreaks.
+#To create strings that span multiple lines, triple single quotes ''' or triple double quotes """ are used to enclose the string.
+#With triple quotes, you can print strings on multiple lines to make text, especially lengthy text, easier to read.
+print("""There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.""")
