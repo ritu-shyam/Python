e2c339d41f02aebda2e747b2d0b3dcb4b0d743d2
ritu-shyam <61557586+ritu-shyam@users.noreply.github.com>
2021-02-02 14:57:59 +0530

    Add files via upload

---
+++ Automate the Boring Stuff with Python (2015)/Chapter 2 : Flow Control/Practice Questions/Practice Question 13.py
@@ -0,0 +1,13 @@
+# 13. Write a short program that prints the numbers 1 to 10 using a for loop.
+# Then write an equivalent program that prints the numbers 1 to 10 using a while loop.
+
+# for loop
+for i in range(1, 11, 1):
+  print(i)
+
+# while loop
+i = 1
+while i < 11:
+  print(i)
+  i = i + 1
+  
