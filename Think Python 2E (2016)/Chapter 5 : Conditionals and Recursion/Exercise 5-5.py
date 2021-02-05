5a3dacdb52c0844c45a283ce5ce63932ae088122
ritu-shyam <61557586+ritu-shyam@users.noreply.github.com>
2021-02-05 23:00:42 +0530

    Add files via upload

---
+++ Think Python 2E (2016)/Chapter 5 : Conditionals and Recursion/Exercise 5-5.py
@@ -0,0 +1,16 @@
+import turtle
+t = turtle.Turtle()
+
+def draw(t, length, n):
+    if n == 0:
+        return
+    angle = 50
+    t.fd(length*n)
+    t.lt(angle)
+    draw(t, length, n-1)
+    t.rt(2*angle)
+    draw(t, length, n-1)
+    t.lt(angle)
+    t.bk(length*n)
+
+draw(t, 10, 6)
