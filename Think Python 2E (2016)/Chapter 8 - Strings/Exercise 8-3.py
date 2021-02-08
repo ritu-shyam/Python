34f4ad0a9c16be5a1b74159ddccbbc86d2c95d4c
ritu-shyam <61557586+ritu-shyam@users.noreply.github.com>
2021-02-08 14:01:54 +0530

    Add files via upload

---
+++ Think Python 2E (2016)/Chapter 8 - Strings/Exercise 8-3.py
@@ -0,0 +1,22 @@
+# Palindrome
+
+word = input('Enter the word')
+
+def first(word):
+    return word[1]
+def last(word):
+    return word[-1]
+def middle(word):
+    return word[2:-1]
+
+f = first(word)
+l = last(word)
+m = middle(word)
+# to get the reverse of the middle string
+r = m[::-1]
+
+def is_palindrome(word):
+    if f == l and m == r:
+        return True
+    else:
+        return False
